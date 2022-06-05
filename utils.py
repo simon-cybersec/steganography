1f8724bf8a659b7f4aaf57b929d493c0d2076795
simon-cybersec <coffeeandcoding@t-online.de>
2022-06-05 19:40:55 +0200

    edited utils

--- utils.py
+++ utils.py
@@ -1,26 +1,27 @@
-def bin_int(x):
+def bin_as_int(x):
     return int(x, 2)
 
 
-def dec_to_string(decimals_list):
-    # Generate a char/string representation of decimals
-    char_list = []
-    for num in decimals_list:
-        char_list.append(chr(num))
+# Generate a char/string representation of decimals
+def dec_to_string(decimals):
+    chars = []
+    for i in decimals:
+        chars.append(chr(i))
+
     # Generate a string representation
     string_representation = ""
-    for c in char_list:
+    for c in chars:
         string_representation += c
+
     # print("string :", end=" ")
     # print(string_representation)
     return string_representation
 
 
-def dec_to_bin(decimals_list):
-    # Generate a binary represantation
-    # print("binary :", end=" ")
+# Generate a binary represantation (string of 0s and 1s) of decimals
+def dec_to_bin(decimals):
     string = ""
-    for x in decimals_list:
+    for x in decimals:
         # print(bin(x), end=" ")
         string += bin(x) + " "
     return string
